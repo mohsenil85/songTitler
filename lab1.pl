88b57a0f535ceabfce91aa9ac3a0eee171b383f0
mohsenil85 <mohsenil85@gmail.com>
2013-02-11 18:45:04 -0700

    i must you have a lot of bitches lil b

--- lab1.pl
+++ lab1.pl
@@ -7,7 +7,7 @@
 #########################################
 
 # Replace the string value of the following variable with your name.
-my $name = "logan";
+my $name = "logan Mohseni";
 print "CSCI Lab 1 submitted by $name\n\n";
 
 # Checks for the argument, fail if none given
@@ -26,18 +26,42 @@ open(INFILE, $ARGV[0]) or die "Cannot open $ARGV[0]: $!.\n";
 # This loops through each line of the file
 while($line = <INFILE>) {
 
-	# This prints each line. You will not want to keep this line.
-	print $line;
+	chomp $line;
+	$line =~ s/^.*<SEP>//; #step 1, grap song title
+	$title = $line;
+	$title =~ s/(feat|\"|\(|\[|\{|\\|\/|\-|\:|\‘|\+|\=|\*).*$//; #step 2, discard anything after these chars
+	$title =~ s/(\_|\?|\¿|\!|\¡|\.|\;|\|\&|\$|\@|\%)//g;  #step 3, remove unwanted punc
+	$title =~ tr/[A-Z]/[a-z]/; #step 4, change to lower case
+	my @array = split( ' ', $title );
+	foreach $word (@array){
+		$i = 0;
+		$bigram = $array[i] . " " . $array[i+1];
+		$i = $i + 1;
+		print "bigram = $bigram\n";    
 
-	# YOUR CODE HERE....
+		#print "$word \n";
+		#print "$array[$word] $array[$word+1]\n";
+		$bigram = "$word2 $word1";
+		$word2 = $word1;
+		$count{$bigram}++;
+	}
+	print "end arr\n\n";
+#foreach $bigram (sort numerically keys %count) {
+#print "$count{$bigram} $bigram\n";
+#}
+
+#sub numerically { # compare two words numerically
+#$count{$b} <=> $count{$a}; # decreasing order
+# $count{$b} <=> $count{$a}; # increasing order
+#} 
 		
-}
+}# end while line in file
 
 # Close the file handle
 close INFILE; 
 
 # At this point you will have finished processing the song title
 # file and have populated your data structure of bigram counts.
-print "File parsed. Bigram model built.";
+print "File parsed. Bigram model built.\n";
 
 # MORE OF YOUR CODE HERE....
